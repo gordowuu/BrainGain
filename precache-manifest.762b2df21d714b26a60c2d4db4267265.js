b95e688d27c124683544a11bad061ba6a799e9ef
gordowuu <gdwu007@gmail.com>
2023-04-20 22:17:59 -0400

    Updates

---
+++ precache-manifest.762b2df21d714b26a60c2d4db4267265.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48125603cf8f5584764a",
+    "url": "/static/css/main.bb12e613.chunk.css"
+  },
+  {
+    "revision": "48125603cf8f5584764a",
+    "url": "/static/js/main.48125603.chunk.js"
+  },
+  {
+    "revision": "d53f7d902f0fd1660d33",
+    "url": "/static/js/1.d53f7d90.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b88e7885f900660ec0f14769e765380e",
+    "url": "/index.html"
+  }
+];
