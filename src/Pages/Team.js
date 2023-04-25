10fe20b30faa7611b5d8673554a1de093bb19155
gordowuu <gdwu007@gmail.com>
2023-04-24 23:29:27 -0400

    team work

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -3,9 +3,9 @@ import React from "react";
 const Team = () => {
     return (
     <React.StrictMode>
-        <div id="team">
+        {/* <div id="team">
             <h1>Brain Gain 2023 Team</h1>   
-            <div class="team-member">
+            <div className="team-member">
                 <img src="assets/img/person_2.jpg" alt="John Doe"></img>
                 <div>
                     <h3>Alex Chen</h3>
@@ -13,7 +13,7 @@ const Team = () => {
                 </div>
             </div>
 
-            <div class="team-member">
+            <div className="team-member">
                 <img src="assets/img/person_3.jpg" alt="Jane Smith"></img>
                 <div>
                     <h3>Gordon Wu</h3>
@@ -21,6 +21,46 @@ const Team = () => {
                 </div>
             </div>
 
+        </div> */}
+        <div id="team">
+            <div className="team-column">
+                <div className="team-card">
+                <img src="assets/img/person_2.jpg" alt="Jane" style="width:100%"></img>
+                <div className="team-container">
+                    <h2>Jane Doe</h2>
+                    <p className="team-title">CEO & Founder</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="team-button">Contact</button></p>
+                </div>
+                </div>
+            </div>
+
+            <div className="team-column">
+                <div className="team-card">
+                <img src="assets/img/person_3.jpg" alt="Mike" style="width:100%"></img>
+                <div className="team-container">
+                    <h2>Mike Ross</h2>
+                    <p className="team-title">Art Director</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="team-button">Contact</button></p>
+                </div>
+                </div>
+            </div>
+            
+            <div className="team-column">
+                <div className="team-card">
+                <img src="assets/img/person_4.jpg" alt="John" style="width:100%"></img>
+                <div className="team-container">
+                    <h2>John Doe</h2>
+                    <p className="team-title">Designer</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="team-button">Contact</button></p>
+                </div>
+                </div>
+            </div>
         </div>
     </React.StrictMode>
         
