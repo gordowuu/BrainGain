2a731cb184b43c206b2a392e85aec8c27f1732a7
gordowuu <gdwu007@gmail.com>
2023-04-26 11:16:42 -0400

    .

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -3,66 +3,136 @@ import React from "react";
 const Team = () => {
     return (
     <React.StrictMode>
-        <h1>Brain Gain 2023 Team</h1>
-        {/* <div id="team">
-            <h1>Brain Gain 2023 Team</h1>   
-            <div className="team-member">
-                <img src="assets/img/person_2.jpg" alt="John Doe"></img>
-                <div>
-                    <h3>Alex Chen</h3>
-                    <p>President</p>
+        <div id="team">
+          <div className="team-content">
+            <div className="team-grid text-center">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="main-title text-center wow fadeIn">
+                      <h3>Meet Our Team</h3>
+                      <div className="underline1"></div>
+                      <div className="underline2"></div>
+                      <p>
+                        our team is so dumb lul, especially chairman !1?#@!@#
+                      </p>
+                    </div>
+                  </div>
                 </div>
+                <div className="row love-row wow fadeIn">
+                  <div className="col-md-4 col-sm-6">
+                    <div className="team-details" data-wow-delay=".1s">
+                      <div className="team-head">
+                        <img
+                          src="assets/img/person_1.jpg"
+                          alt="name"
+                        />
+                      </div>
+                      <div className="team-bottom">
+                        <h3>Name</h3>
+                        <div className="underline1"></div>
+                        <div className="underline2"></div>
+                        <p>
+                          Role
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4 col-sm-6">
+                    <div className="team-details" data-wow-delay=".1s">
+                      <div className="team-head">
+                        <img
+                          src="assets/img/person_2.jpg"
+                          alt="name"
+                        />
+                      </div>
+                      <div className="team-bottom">
+                        <h3>Name</h3>
+                        <div className="underline1"></div>
+                        <div className="underline2"></div>
+                        <p>
+                          Role
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4 col-sm-6">
+                    <div className="team-details" data-wow-delay=".1s">
+                      <div className="team-head">
+                        <img
+                          src="assets/img/person_4.jpg"
+                          alt="name"
+                        />
+                      </div>
+                      <div className="team-bottom">
+                        <h3>Name</h3>
+                        <div className="underline1"></div>
+                        <div className="underline2"></div>
+                        <p>
+                          Role
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4 col-sm-6">
+                    <div className="team-details" data-wow-delay=".1s">
+                      <div className="team-head">
+                        <img
+                          src="assets/img/person_3.jpg"
+                          alt="name"
+                        />
+                      </div>
+                      <div className="team-bottom">
+                        <h3>Name</h3>
+                        <div className="underline1"></div>
+                        <div className="underline2"></div>
+                        <p>
+                          Role
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4 col-sm-6">
+                    <div className="team-details" data-wow-delay=".1s">
+                      <div className="team-head">
+                        <img
+                          src="assets/img/person_1.jpg"
+                          alt="name"
+                        />
+                      </div>
+                      <div className="team-bottom">
+                        <h3>Name</h3>
+                        <div className="underline1"></div>
+                        <div className="underline2"></div>
+                        <p>
+                          Role
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4 col-sm-6">
+                    <div className="team-details" data-wow-delay=".1s">
+                      <div className="team-head">
+                        <img
+                          src="assets/img/person_2.jpg"
+                          alt="name"
+                        />
+                      </div>
+                      <div className="team-bottom">
+                        <h3>Name</h3>
+                        <div className="underline1"></div>
+                        <div className="underline2"></div>
+                        <p>
+                          Role
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-
-            <div className="team-member">
-                <img src="assets/img/person_3.jpg" alt="Jane Smith"></img>
-                <div>
-                    <h3>Gordon Wu</h3>
-                    <p>Chief Technology Officer</p>
-                </div>
             </div>
-
-        </div> */}
-        {/* <div id="team">
-            <div className="team-column">
-                <div className="team-card">
-                <img src="assets/img/person_2.jpg" alt="Jane" style="width:100%"></img>
-                <div className="team-container">
-                    <h2>Jane Doe</h2>
-                    <p className="team-title">CEO & Founder</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                    <p>example@example.com</p>
-                    <p><button className="team-button">Contact</button></p>
-                </div>
-                </div>
-            </div>
-
-            <div className="team-column">
-                <div className="team-card">
-                <img src="assets/img/person_3.jpg" alt="Mike" style="width:100%"></img>
-                <div className="team-container">
-                    <h2>Mike Ross</h2>
-                    <p className="team-title">Art Director</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                    <p>example@example.com</p>
-                    <p><button className="team-button">Contact</button></p>
-                </div>
-                </div>
-            </div>
-            
-            <div className="team-column">
-                <div className="team-card">
-                <img src="assets/img/person_4.jpg" alt="John" style="width:100%"></img>
-                <div className="team-container">
-                    <h2>John Doe</h2>
-                    <p className="team-title">Designer</p>
-                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                    <p>example@example.com</p>
-                    <p><button className="team-button">Contact</button></p>
-                </div>
-                </div>
-            </div>
-        </div> */}
+        </div>
     </React.StrictMode>
         
     );
